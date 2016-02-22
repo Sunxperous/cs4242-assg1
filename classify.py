72dc240ff336faf20d98505e93339e44bc1df49a
Sun Wang Jun <sunxperous@gmail.com>
2016-02-22 21:23:16 +0800

    Refine Index initialization and methods

--- classify.py
+++ classify.py
@@ -8,7 +8,7 @@ index = Index('testing')
 
 knn = neighbors.KNeighborsClassifier(weights='uniform')
 sample = [v for k, v in index.tweet_features.items()]
-target = [v for k, v in index.development_labels.items()]
+target = [v for k, v in index.tweet_labels.items()]
 knn.fit(sample, target)
 
 
--- index.py
+++ index.py
@@ -11,27 +11,52 @@ from utility import paths, punctuation_set, stemmer, stopwords_set
 
 
 class Index:
-    def __init__(self, csv_type):
-        # Machine learning data.
-        self.feature_set = {}
+    """
+    Attributes:
+        feature_set: All the terms used to generate the feature vectors.
+        train: Whether to add terms to the feature_set or not.
+        tweet_labels: Actual topic and sentiment labels for given tweet.
+        tweet_data: Raw tweet data.
+        tweet_features: Tweet data transformed into feature vectors.
+    """
+    def __init__(self, csv_type, feature_set=None):
+        """
+        Args:
+            csv_type: Either 'development', 'training', or 'testing'.
+            feature_set: Whether to use a fixed feature set, or None.
+        """
 
-        self.development_labels = self.read_csv(csv_type)
-        self.tweets_data = self.read_tweets(paths['directories'].get('tweets'))
-        self.tweet_features = self.generate_feature_vectors(self.tweets_data)
+        if feature_set is None:
+            self.feature_set = {}
+            self.train = True
+            print('indexing with training...')
+        else:
+            self.feature_set = feature_set
+            self.train = False
+            print('indexing without training...')
+
+        print('reading csv ' + csv_type + '...')
+        self.tweet_labels = self.read_csv(csv_type)
+        print('labelled ' + str(len(self.tweet_labels)) + ' tweets')
+        print('reading tweets...')
+        self.tweet_data = self.read_tweets(paths['directories'].get('tweets'))
+        print('read ' + str(len(self.tweet_data)) + ' tweets')
+        print('generating feature vectors...')
+        self.tweet_features = self.generate_feature_vectors(self.tweet_data)
+        print('generated ' + str(len(self.tweet_features)) + ' feature vectors')
+        print('indexing complete.')
 
     def read_csv(self, csv_name):
-        print('reading csv: ' + csv_name + '...')
-        csv_labels = {}
+        tweet_labels = {}
 
         with open(paths['files'][csv_name]) as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
             next(csv_reader)  # Skip header row.
             for row in csv_reader:
                 label_id = label.ids[row[0]][row[1]]
-                csv_labels[row[2]] =  label_id
+                tweet_labels[row[2]] =  label_id
 
-        print('done reading csv')
-        return csv_labels
+        return tweet_labels
 
     def process_tweet(self, json_data):
         text = json_data.get('text')
@@ -56,30 +81,28 @@ class Index:
         # Add token to feature_set.
         # TODO: Maybe move this part out of this method.
         for x in stemmed:
-            if x not in self.feature_set:
+            if x not in self.feature_set and self.train:
                 self.feature_set[x] = len(self.feature_set)
 
         return stemmed
 
     def read_tweets(self, dir_name):
-        print('reading tweets...')
         json_tweets = {}
 
+        # TODO: Iterate on tweet_labels list instead of iterating on directory.
         for f in listdir(dir_name):
             full_path = path.join(dir_name, f)
 
             tweet_id, f_ext = path.splitext(full_path)
             if (path.isfile(full_path) and f_ext == '.json'
-                    and tweet_id[tweet_id.rfind('/')+1:] in self.development_labels):
+                    and tweet_id[tweet_id.rfind('/')+1:] in self.tweet_labels):
                 with open(full_path) as json_file:
                     json_data = json.load(json_file)
                     json_tweets[json_data['id']] = self.process_tweet(json_data)
 
-        print('done reading tweets')
         return json_tweets
 
     def generate_feature_vectors(self, tweet_data):
-        print('generating feature vectors...')
         tweet_features = {}
 
         for tweet_id, data in tweet_data.items():
@@ -91,6 +114,5 @@ class Index:
 
             tweet_features[tweet_id] = vector
 
-        print('done generating feature vectors')
         return tweet_features
 
