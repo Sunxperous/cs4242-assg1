c867c03d4693d0190d1fff9d9e6776debb8df3a8
Seokhoon Lee <seokhoon.lee.9393@gmail.com>
2016-02-22 21:24:45 +0800

    Add SVM

--- classify.py
+++ classify.py
@@ -1,15 +1,23 @@
 from sklearn import neighbors
-
+from sklearn import svm
 
 from index import Index
 
 
 index = Index('testing')
 
-knn = neighbors.KNeighborsClassifier(weights='uniform')
 sample = [v for k, v in index.tweet_features.items()]
 target = [v for k, v in index.development_labels.items()]
+
+knn = neighbors.KNeighborsClassifier(weights='uniform')
 knn.fit(sample, target)
+
+svc = svm.SVC(kernel='linear')
+# linear kernel is used when feature size is big (~10,000) and sample size is moderate (5000)
+# C is default 1.0, decrease if overfitting, increase if underfitting
+# if uneven data result, try adding (class_weight='balanced')
+# scaling and normalization is highly recommended
+svc.fit(sample, target) 
 
 
 
